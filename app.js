396f315e320e8cb81cbebb0558921e77a3d4d61b
kimseongwooo <93570839+kimseongwooo@users.noreply.github.com>
2023-03-10 17:36:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,12 +10,7 @@ const helmet = require('helmet');
 const hpp = require('hpp');
 const redis = require('redis');
 
-
 dotenv.config();
-const redisClient = redis.createClient({
-  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-  password: process.env.REDIS_PASSWORD,
-});
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
@@ -59,6 +54,7 @@ app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+
 const sessionOption = {
   resave: false,
   saveUninitialized: false,
@@ -67,13 +63,23 @@ const sessionOption = {
     httpOnly: true,
     secure: false,
   },
-  store: redisClient,
 };
+
 if (process.env.NODE_ENV === 'production') {
   sessionOption.proxy = true;
   // sessionOption.cookie.secure = true;
 }
-app.use(session(sessionOption));
+
+app.use((req, res, next) => {
+  const redisClient = redis.createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    password: process.env.REDIS_PASSWORD,
+  });
+  sessionOption.store = redisClient;
+  const sessionMiddleware = session(sessionOption);
+  sessionMiddleware(req, res, next);
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 
